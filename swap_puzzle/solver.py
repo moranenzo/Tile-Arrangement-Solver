ad066005ac13a3ba8a3f97c54e9cd95b7d387c39
yanis <alexis.dahlen@ensae.fr>
2024-02-11 22:10:25 +0000

    Ajout

--- swap_puzzle/solver.py
+++ swap_puzzle/solver.py
@@ -1,6 +1,7 @@
 from grid import Grid
 
-class Solver(): 
+
+class Solver():
     """
     A solver class, to be implemented.
     """
@@ -10,7 +11,27 @@ class Solver():
         Solves the grid and returns the sequence of swaps at the format 
         [((i1, j1), (i2, j2)), ((i1', j1'), (i2', j2')), ...]. 
         """
-        # TODO: implement this function (and remove the line "raise NotImplementedError").
-        # NOTE: you can add other methods and subclasses as much as necessary. The only thing imposed is the format of the solution returned.
-        raise NotImplementedError
+        final_list = []
+        ind = 1
+        m = self.m
+        n = self.n
 
+        while ind <= n*m:
+            temp_list = []
+            init_pos = self.position(ind)
+            final_pos = (ind//n, ind % n - 1)
+
+            sens = 1 - 2*(final_pos[1] <= init_pos[1])  # prend la valeur 1 ou -1, permet de déterminer dans quel sens on fait les échanges
+            for j in range(final_pos[1], init_pos[1], sens):
+                temp_list.append((init_pos[0], j), (init_pos[0], j+1))
+
+            for i in range(final_pos[0], init_pos[0], -1):  #on tri linéairement donc init_pos[1] est nécessairement plus grand que final_pos[1]
+                temp_list.append((i, final_pos[1]), (i+1, final_pos[1]))
+      
+            self.swap_seq(temp_list)
+
+            final_list += temp_list
+
+            ind += 1
+        
+        return final_list
