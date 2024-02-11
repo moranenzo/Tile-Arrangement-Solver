8e46d8276ca93e07b4b30f88dd91d31463d484ae
yanis <alexis.dahlen@ensae.fr>
2024-02-11 22:09:16 +0000

    ajout du dico graph

--- swap_puzzle/main.py
+++ swap_puzzle/main.py
@@ -1,9 +1,13 @@
+import sys
+sys.path.append("swap_puzzle/")
+
 from grid import Grid
+from graph import Graph
 
 g = Grid(2, 3)
 print(g)
 
-data_path = "../input/"
+data_path = "/home/onyxia/ensae-prog24/input/"
 file_name = data_path + "grid0.in"
 
 print(file_name)
@@ -11,4 +15,18 @@ print(file_name)
 g = Grid.grid_from_file(file_name)
 print(g)
 
-#test
+
+
+# Création du graph
+
+'Ici src est la grille source'
+m = src.m
+n = src.n
+
+dst = [[i*n + j + 1 for j in range(n)] for i in range(n)]
+
+graph = Graph(Grid(m,n).generate())
+
+graph.graph = dict([(n, n.neighbors()) for n in graph.nodes])
+
+graph.bfs(src, dst)
