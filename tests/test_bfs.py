c6e73e94091e09031629859123b293723bddc565
yanis <alexis.dahlen@ensae.fr>
2024-03-10 21:36:32 +0000

    Ajout d'un nouveau test

--- tests/test_bfs.py
+++ tests/test_bfs.py
@@ -9,3 +9,14 @@ for n in test_bfs.nodes:
     node= Grid(grille_test.m, grille_test.n, [ list(k) for k in n] )
     test_bfs.graph[n]=[Grid(2,2,neighbor) for neighbor in node.neighbors()]
 test_bfs.bfs(grille_test,grille_sorted) 
+
+grille_test= Grid(3,2,[[1,6],[2,3],[4,5]])
+test_bfs=Graph(grille_test.generate())
+grille_sorted= Grid(3,2,[[1,2],[3,4],[5,6]])
+
+
+for n in test_bfs.nodes:
+    node= Grid(grille_test.m, grille_test.n, [ list(k) for k in n] )
+    test_bfs.graph[n]=[Grid(3,2,neighbor) for neighbor in node.neighbors()]
+test_bfs.bfs(grille_test,grille_sorted) 
+test_bfs.bfs_improved(grille_test,grille_sorted)
