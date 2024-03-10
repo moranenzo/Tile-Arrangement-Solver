d8e2fc6a84b3b777ecc64f78cbd9320b276be643
yanis <alexis.dahlen@ensae.fr>
2024-03-10 21:36:55 +0000

    Ajout d'un nouveau test

--- tests/test_A_star.py
+++ tests/test_A_star.py
@@ -8,3 +8,12 @@ for n in test_A_star.nodes:
     node= Grid(grille_test.m, grille_test.n, [ list(k) for k in n] )
     test_A_star.graph[n]=[Grid(2,2,neighbor) for neighbor in node.neighbors()]
 test_A_star.A_star(grille_test,grille_sorted)
+
+grille_test= Grid(3,2,[[1,6],[2,3],[4,5]])
+test_bfs=Graph(grille_test.generate())
+grille_sorted= Grid(3,2,[[1,2],[3,4],[5,6]])
+
+test_A_star=Graph(grille_test.generate())
+for n in test_A_star.nodes:
+    node= Grid(grille_test.m, grille_test.n, [ list(k) for k in n] )
+    test_A_star.graph[n]=[Grid(3,2,neighbor) for neighbor in node.neighbors()]
