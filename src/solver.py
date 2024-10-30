385af1b12ea5074f035fe8376d68750c5ba219e3
Enzo <120461912+moranenzo@users.noreply.github.com>
2024-10-30 22:05:44 +0100

    Update solver.py

--- src/solver.py
+++ src/solver.py
@@ -1,37 +1,45 @@
 from grid import Grid
 
+class Solver:
+    """
+    Solver class for the tile arrangement puzzle.
+    """
 
-class Solver():
-    """
-    A solver class, to be implemented.
-    """
-    
     def get_solution(self):
         """
-        Solves the grid and returns the sequence of swaps at the format 
-        [((i1, j1), (i2, j2)), ((i1', j1'), (i2', j2')), ...]. 
+        Solves the grid puzzle and returns a list of swap operations needed
+        to reach the sorted state.
+
+        Returns:
+        --------
+        list[tuple[tuple[int, int], tuple[int, int]]]
+            List of swaps in the format [((i1, j1), (i2, j2)), ((i1', j1'), (i2', j2')), ...],
+            where each tuple represents a pair of cells to swap.
         """
-        final_list = []
-        ind = 1
-        m = self.m
-        n = self.n
+        solution_steps = []
+        tile_number = 1
+        m, n = self.m, self.n
 
-        while ind <= n*m:
-            temp_list = []
-            init_pos = self.position(ind)
-            final_pos = (ind//n, ind % n - 1)
+        while tile_number <= m * n:
+            swap_sequence = []
+            current_pos = self.position(tile_number)
+            target_pos = (tile_number // n, tile_number % n - 1)
 
-            sens = 1 - 2*(final_pos[1] <= init_pos[1])  # prend la valeur 1 ou -1, permet de déterminer dans quel sens on fait les échanges
-            for j in range(final_pos[1], init_pos[1], sens):
-                temp_list.append((init_pos[0], j), (init_pos[0], j+1))
+            # Determine swap direction: 1 for right, -1 for left
+            direction = 1 if target_pos[1] > current_pos[1] else -1
 
-            for i in range(final_pos[0], init_pos[0], -1):  #on tri linéairement donc init_pos[1] est nécessairement plus grand que final_pos[1]
-                temp_list.append((i, final_pos[1]), (i+1, final_pos[1]))
-      
-            self.swap_seq(temp_list)
+            # Horizontal swaps to align columns
+            for col in range(current_pos[1], target_pos[1], direction):
+                swap_sequence.append(((current_pos[0], col), (current_pos[0], col + direction)))
 
-            final_list += temp_list
+            # Vertical swaps to align rows
+            for row in range(current_pos[0], target_pos[0], -1):
+                swap_sequence.append(((row, target_pos[1]), (row + 1, target_pos[1])))
 
-            ind += 1
-        
-        return final_list
+            # Apply swaps and add to solution list
+            self.swap_seq(swap_sequence)
+            solution_steps.extend(swap_sequence)
+
+            tile_number += 1
+
+        return solution_steps
