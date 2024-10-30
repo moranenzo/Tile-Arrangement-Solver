42632b8a919a9afad3fc966e3960a93c37990a60
Enzo <120461912+moranenzo@users.noreply.github.com>
2024-10-30 22:03:56 +0100

    Update graph.py

--- src/graph.py
+++ src/graph.py
@@ -1,73 +1,68 @@
 """
-This is the graph module. It contains a minimalistic Graph class.
+This module defines the Graph class for undirected graphs represented by adjacency lists.
 """
+
 from grid import Grid
 
 
 class Graph:
     """
-    A class representing undirected graphs as adjacency lists.
+    Represents an undirected graph using adjacency lists.
 
     Attributes:
     -----------
-    nodes: NodeType
-        A list of nodes. Nodes can be of any immutable type, e.g., integer, float, or string.
-        We will usually use a list of integers 1, ..., n.
-    graph: dict
-        A dictionnary that contains the adjacency list of each node in the form
-        graph[node] = [neighbor1, neighbor2, ...]
-    nb_nodes: int
-        The number of nodes.
-    nb_edges: int
-        The number of edges.
-    edges: list[tuple[NodeType, NodeType]]
-        The list of all edges
+    nodes : list
+        A list of nodes, which can be any immutable type (e.g., integer, string).
+    graph : dict
+        A dictionary storing adjacency lists, where graph[node] = [neighbor1, neighbor2, ...].
+    nb_nodes : int
+        The number of nodes in the graph.
+    nb_edges : int
+        The number of edges in the graph.
+    edges : list[tuple]
+        A list of all edges in the graph.
     """
 
-    def __init__(self, nodes=[]):
+    def __init__(self, nodes=None):
         """
-        Initializes the graph with a set of nodes, and no edges.
+        Initializes the graph with a set of nodes and no edges.
 
         Parameters:
         -----------
-        nodes: list, optional
-            A list of nodes. Default is empty.
+        nodes : list, optional
+            A list of nodes. Default is an empty list.
         """
-        self.nodes = nodes
-        self.graph = dict([(n, []) for n in nodes])
-        self.nb_nodes = len(nodes)
+        self.nodes = nodes if nodes is not None else []
+        self.graph = {node: [] for node in self.nodes}
+        self.nb_nodes = len(self.nodes)
         self.nb_edges = 0
         self.edges = []
-        
+
     def __str__(self):
         """
-        Prints the graph as a list of neighbors for each node (one per line)
+        Returns a string representation of the graph with each node and its neighbors.
         """
         if not self.graph:
-            output = "The graph is empty"
-        else:
-            output = f"The graph has {self.nb_nodes} nodes and {self.nb_edges} edges.\n"
-            for source, destination in self.graph.items():
-                output += f"{source}-->{destination}\n"
+            return "The graph is empty"
+        output = f"The graph has {self.nb_nodes} nodes and {self.nb_edges} edges.\n"
+        for node, neighbors in self.graph.items():
+            output += f"{node} --> {neighbors}\n"
         return output
 
     def __repr__(self):
         """
-        Returns a representation of the graph with number of nodes and edges.
+        Returns a summary of the graph showing the number of nodes and edges.
         """
-        return f"<graph.Graph: nb_nodes={self.nb_nodes}, nb_edges={self.nb_edges}>"
+        return f"<Graph: nb_nodes={self.nb_nodes}, nb_edges={self.nb_edges}>"
 
     def add_edge(self, node1, node2):
         """
-        Adds an edge to the graph. Graphs are not oriented, hence an edge is added to the adjacency list of both end nodes.
-        When adding an edge between two nodes, if one of the ones does not exist it is added to the list of nodes.
+        Adds an undirected edge between two nodes. If a node does not exist, it is added.
 
         Parameters:
         -----------
-        node1: NodeType
-            First end (node) of the edge
-        node2: NodeType
-            Second end (node) of the edge
+        node1, node2 : Any immutable type
+            The two nodes to connect by an edge.
         """
         if node1 not in self.graph:
             self.graph[node1] = []
@@ -85,199 +80,152 @@ class Graph:
 
     def bfs(self, src, dst):
         """
-        Finds a shortest path from src to dst by BFS.
+        Finds the shortest path from src to dst using Breadth-First Search (BFS).
 
         Parameters:
         -----------
-        src: Grid
-            The source node.
-        dst: Grid
-            The destination node.
+        src : Grid
+            The source grid configuration.
+        dst : Grid
+            The destination grid configuration.
 
-        Output:
-        -------
-        path: list[tuple] | None
-            The shortest path from src to dst. Returns None if dst is not reachable from src
+        Returns:
+        --------
+        list[tuple] | None
+            A list representing the shortest path from src to dst, or None if no path exists.
         """
-
-        # Initialisation
-        graph = self.graph
         queue = [src]
-        father = {}
+        parent_map = {src.to_tuple(): None}
         found_path = False
-
-        # On détermine un chemin reliant src et dst
-        for node in queue:
-            father[node.hash()] = None
 
         while queue and not found_path:
             current = queue.pop(0)
-            if current.hash() == dst.hash():
-                found_path = True  # permet de sortir de la boucle while
-
+            if current.to_tuple() == dst.to_tuple():
+                found_path = True
             else:
-                neighbors = graph[current.hash()]  # liste des voisins de current
-
-                for neighbor in neighbors:
-
-                    # On vérifie que neighbor n'a pas déjà été traité (i.e. visité ou ajouté à la file)
-                    if (neighbor.hash() not in father) and (neighbor not in queue):
+                for neighbor in self.graph[current.to_tuple()]:
+                    if neighbor.to_tuple() not in parent_map:
                         queue.append(neighbor)
-                        father[neighbor.hash()] = current
+                        parent_map[neighbor.to_tuple()] = current
 
-        # On vérifie qu'un chemin entre src et dst a bien été trouvé
         if not found_path:
             return None
 
-        # Construction du chemin reliant src et dst à partir du dico father
-        child = dst
-        chemin = []
-        while child:
-            chemin = [child.hash()] + chemin
-            child = father[child.hash()]
+        path = []
+        step = dst
+        while step:
+            path.insert(0, step.to_tuple())
+            step = parent_map[step.to_tuple()]
 
-        return chemin
+        return path
 
     def bfs_improved(self, src, dst):
         """
-        Finds a shortest path from src to dst by BFS.
+        An optimized BFS that dynamically generates neighbors.
 
         Parameters:
         -----------
-        src: Grid
-            The source node.
-        dst: Grid
-            The destination node.
+        src : Grid
+            The source grid configuration.
+        dst : Grid
+            The destination grid configuration.
 
-        Output:
-        -------
-        path: list[tuple] | None
-            The shortest path from src to dst. Returns None if dst is not reachable from src
+        Returns:
+        --------
+        list[tuple] | None
+            A list representing the shortest path from src to dst, or None if no path exists.
         """
-
-        # Initialisation
         queue = [src]
-        father = {}
+        parent_map = {src.to_tuple(): None}
         found_path = False
-
-        # On détermine un chemin reliant src et dst
-        for node in queue:
-            father[node.hash()] = None
 
         while queue and not found_path:
             current = queue.pop(0)
-            if current.hash() == dst.hash():
+            if current.to_tuple() == dst.to_tuple():
                 found_path = True
-
             else:
-                matrice_neighbors = current.neighbors()
-                for matrice_neighbor in matrice_neighbors:
-                    # Construction de la grid voisine à current
-                    neighbor = Grid(src.m, src.n, matrice_neighbor)
-
-                    # On vérifie que neighbor n'a pas déjà été traité (i.e. visité ou ajouté à la file)
-                    if (neighbor.hash() not in father) and (neighbor not in queue):
+                for neighbor_state in current.neighbors():
+                    neighbor = Grid(src.m, src.n, neighbor_state)
+                    if neighbor.to_tuple() not in parent_map:
                         queue.append(neighbor)
-                        father[neighbor.hash()] = current
+                        parent_map[neighbor.to_tuple()] = current
 
-        # On vérifie qu'un chemin entre src et dst a bien été trouvé
         if not found_path:
             return None
 
-        # Construction du chemin reliant src et dst à partir du dico father
-        child = dst
-        chemin = []
-        while child:
-            chemin = [child.hash()] + chemin
-            child = father[child.hash()]
-
-        return chemin
-
-
-    def A_star(self, src, dst):
-        open_list = [(src, 0, src.manhattan_dist(dst), [])]  # içi la liste source n'a pas de père.
-        closed_list = []
         path = []
-        # On lance une première fois l'algorithme afin d'éviter une erreur au niveau de if neighbor != open_list[0][3].state:
-        if src.state != dst.state:
-            closed_list.append(open_list[0])
-            neighbors = open_list[0][0].neighbors()
-            for neighbor in neighbors:
-                open_list.append((Grid(src.m, src.n, neighbor), open_list[0][1]+1, open_list[0][1]+1+Grid(src.m, src.n, neighbor).manhattan_dist(dst), open_list[0][0]))
-            open_list.pop(0)
-            open_list.sort(key=lambda x: x[2])
-              
-        while open_list:
-            if open_list[0][0].state == dst.state:       
-                path.append(dst.hash())
-                father = open_list[0][3]
-                while father:  # Permet de remonter jusqu'a la grille source qui est la seule à ne pas avoir de père.
-                    
-                    for acn in closed_list:  # acn pour already closed nodes.
-                            
-                        if (father != []) and (acn[0].state == father.state):
-                            open_list[0] = acn
-                            path = [acn[0].hash()] + path
-                            father = open_list[0][3]
-                open_list = []  # Permet de sortir de la première boucle while.
-            else:
-                icl = False  # icl pour in closed list.
-                for i in range(0, len(closed_list)): 
-                    if closed_list[i][0].state == open_list[0][0].state:
-                        icl = True
-                        if closed_list[i][2] > open_list[0][2]:  # Si  l'heuristique du noeud déjà visité est meilleure alors on le remplace  dans la liste afin d'obtenir le meilleur père possible. 
-                            closed_list[i] = open_list[0]
+        step = dst
+        while step:
+            path.insert(0, step.to_tuple())
+            step = parent_map[step.to_tuple()]
 
-                            neighbors = open_list[0][0].neighbors()
-                            for neighbor in neighbors:
-                                open_list.append((Grid(src.m, src.n, neighbor), open_list[0][1]+1, open_list[0][1]+1+Grid(src.m, src.n, neighbor).manhattan_dist(dst), open_list[0][0]))  
-                                # on définit le nouveau coût de ses voisins ainsi que la nouvelle heuristique.
-                        open_list.pop(0)
-                        open_list.sort(key=lambda x: x[2])  # Permet de trier la grille selon les heuristiques.
-                        
-                if not icl:  # Si icl vaut True on a déjà effectué les opérations nécessaires.
-                    closed_list.append(open_list[0])
-                    neighbors = open_list[0][0].neighbors()
-                    for neighbor in neighbors:
-                         if neighbor != open_list[0][3].state: 
-                            """
-                            On n' implémente pas le père de la grille comme un fils afin d'éviter une boucle infinie
-                            pour les petites grilles dans la création du chemin vers destination 
-                            """
-                            open_list.append((Grid(src.m, src.n, neighbor), open_list[0][1]+1, open_list[0][1]+1+Grid(src.m, src.n, neighbor).manhattan_dist(dst), open_list[0][0]))
-                    open_list.pop(0)
-                    open_list.sort(key=lambda x: x[2])
         return path
 
-    @classmethod
-    def graph_from_file(cls, file_name):
+    def a_star(self, src, dst):
         """
-        Reads a text file and returns the graph as an object of the Graph class.
-
-        The file should have the following format:
-            The first line of the file is 'n m'
-            The next m lines have 'node1 node2'
-        The nodes (node1, node2) should be named 1..n
+        Finds the shortest path from src to dst using the A* algorithm with Manhattan distance.
 
         Parameters:
         -----------
-        file_name: str
-            The name of the file
+        src : Grid
+            The source grid configuration.
+        dst : Grid
+            The destination grid configuration.
 
-        Outputs:
+        Returns:
+        --------
+        list[tuple]
+            A list representing the optimal path from src to dst.
+        """
+        open_list = [(src, 0, src.manhattan_distance(dst), None)]
+        closed_list = []
+        path = []
+
+        while open_list:
+            current, g_cost, _, parent = open_list.pop(0)
+            if current.to_tuple() == dst.to_tuple():
+                while parent:
+                    path.insert(0, current.to_tuple())
+                    current = parent
+                    parent = next((node[3] for node in closed_list if node[0] == current), None)
+                return path
+
+            closed_list.append((current, g_cost, g_cost + current.manhattan_distance(dst), parent))
+
+            for neighbor_state in current.neighbors():
+                neighbor = Grid(src.m, src.n, neighbor_state)
+                neighbor_tuple = neighbor.to_tuple()
+                if any(neighbor_tuple == n[0].to_tuple() and g_cost < n[1] for n in closed_list):
+                    continue
+
+                if not any(neighbor_tuple == n[0].to_tuple() for n in open_list):
+                    open_list.append((neighbor, g_cost + 1, g_cost + 1 + neighbor.manhattan_distance(dst), current))
+                    open_list.sort(key=lambda x: x[2])
+
+        return path
+
+    @classmethod
+    def from_file(cls, file_name):
+        """
+        Reads a text file and returns a Graph object.
+
+        The file format should be:
+            - First line: "n m" where n is the number of nodes, m is the number of edges
+            - Next m lines: "node1 node2" specifying each edge
+
+        Parameters:
         -----------
-        graph: Graph
-            An object of the class Graph with the graph from file_name.
+        file_name : str
+            Path to the file containing the graph.
+
+        Returns:
+        --------
+        Graph : A Graph object initialized with the file data.
         """
         with open(file_name, "r") as file:
             n, m = map(int, file.readline().split())
-            graph = Graph(range(1, n+1))
+            graph = Graph(range(1, n + 1))
             for _ in range(m):
-                edge = list(map(int, file.readline().split()))
-                if len(edge) == 2:
-                    node1, node2 = edge
-                    graph.add_edge(node1, node2)  # will add dist=1 by default
-                else:
-                    raise Exception("Format incorrect")
+                node1, node2 = map(int, file.readline().split())
+                graph.add_edge(node1, node2)
         return graph
-
